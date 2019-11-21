6ef10db42235212c990325a25b09ee15eee01c91
BizzBizzer <56912320+BizzBizzer@users.noreply.github.com>
2019-11-21 11:01:21 +0100

    Unity Build

---
+++ New Unity Project/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public GameObject[] camPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(camPos[0].transform.position.x, camPos[0].transform.position.y, camPos[0].transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
