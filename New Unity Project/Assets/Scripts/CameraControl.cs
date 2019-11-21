606d2fd52620e025b83eda556d9f20fba8233e50
BizzBizzer <56912320+BizzBizzer@users.noreply.github.com>
2019-11-21 15:30:02 +0100

    Camera-Movement

--- New Unity Project/Assets/Scripts/CameraControl.cs
+++ New Unity Project/Assets/Scripts/CameraControl.cs
@@ -12,6 +12,7 @@ public class CameraControl : MonoBehaviour
     void Start()
     {
         transform.position = new Vector3(camPos[0].transform.position.x, camPos[0].transform.position.y, camPos[0].transform.position.z);
+        transform.eulerAngles = new Vector3(transform.localRotation.x + 35,transform.localRotation.y + 45, 0);
     }
 
     // Update is called once per frame
@@ -19,7 +20,9 @@ public class CameraControl : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            
+
+            transform.position = new Vector3(camPos[1].transform.position.x, camPos[1].transform.position.y, camPos[1].transform.position.z);
+            transform.eulerAngles = new Vector3(30, -45, 0);
         }
     }
 }
