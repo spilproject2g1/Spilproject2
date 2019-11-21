2c7f593e6eabfe3cab167ad66c2401b6df02db0a
BizzBizzer <56912320+BizzBizzer@users.noreply.github.com>
2019-11-21 11:30:23 +0100

    In Progress

--- New Unity Project/Assets/Scripts/CameraControl.cs
+++ New Unity Project/Assets/Scripts/CameraControl.cs
@@ -17,6 +17,9 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+        }
     }
 }
