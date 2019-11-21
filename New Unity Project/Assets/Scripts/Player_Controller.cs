caddf600da775db458a0fbfd38fd50b59b8a157a
Andreasjuantwothree <56953776+Andreasjuantwothree@users.noreply.github.com>
2019-11-21 12:46:33 +0100

    MoveSystem

---
+++ New Unity Project/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+
+    public Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+
+    Vector2 movement;
+
+
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+}
