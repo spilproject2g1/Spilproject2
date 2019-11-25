422f7e6da78a46a8ae1bfd336267b08f620a6c9c
Andreasjuantwothree <56953776+Andreasjuantwothree@users.noreply.github.com>
2019-11-25 13:24:01 +0100

    Reset scene

---
+++ New Unity Project/Assets/Scripts/Reset_Scene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Reset_Scene : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+
+            SceneManager.LoadScene("SampleScene");
+    }
+
+}
+
+
