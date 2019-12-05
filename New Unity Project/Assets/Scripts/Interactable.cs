93554be2c8b3717363f301c4a13624bcf1256c4c
BizzBizzer <56912320+BizzBizzer@users.noreply.github.com>
2019-12-05 14:12:57 +0100

    interactable WIP

---
+++ New Unity Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Player_ControllerV2 playerCtrl;
+    public GameObject[] interactableObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCtrl = FindObjectOfType<Player_ControllerV2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
