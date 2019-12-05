58198055903356f373620cb4c3854017d926a5d2
BizzBizzer <56912320+BizzBizzer@users.noreply.github.com>
2019-12-05 15:34:31 +0100

    interactable WIP

--- New Unity Project/Assets/Scripts/Interactable.cs
+++ New Unity Project/Assets/Scripts/Interactable.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    public bool canInteract = false;
+    public bool isInteracting = false;
     private Player_ControllerV2 playerCtrl;
-    public GameObject[] interactableObject;
+    public GameObject inspectCam;
     // Start is called before the first frame update
     void Start()
     {
+        inspectCam.gameObject.SetActive(false);
         playerCtrl = FindObjectOfType<Player_ControllerV2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x - playerCtrl.transform.position.x < 2.5f && transform.position.z - playerCtrl.transform.position.z < 2.5f)
+        {
+            canInteract = true;
+        }
+        else
+        {
+            canInteract = false;
+        }
+
+        if (canInteract == true)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                interact();
+            }
+        }
+    }
+
+    void interact()
+    {
+        isInteracting = true;
+        inspectCam.gameObject.SetActive(true);
+
     }
 }
--- New Unity Project/Assets/Scripts/Player_ControllerV2.cs
+++ New Unity Project/Assets/Scripts/Player_ControllerV2.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class Player_ControllerV2 : MonoBehaviour
 {
     Rigidbody rb;
-    
     Transform camara;
-
     Vector3 movement;
 
+    Interactable interactScript;
 
     public float speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        interactScript = FindObjectOfType<Interactable>();
         rb = GetComponent<Rigidbody>();
         camara = FindObjectOfType<Camera>().transform;
     }
@@ -23,10 +23,14 @@ public class Player_ControllerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(interactScript.isInteracting == false)
+        {
         movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * speed * Time.deltaTime;
         movement = camara.rotation * movement;
         movement.y = rb.velocity.y;
         rb.velocity = movement;
+        }
+
 
         if (Input.GetKeyDown(KeyCode.F))
         {
